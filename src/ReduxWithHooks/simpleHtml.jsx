9ef1d7aab5c9e595d2f73e20d26a489cbc68f36f
zeus <zeus@pop-os.localdomain>
2020-01-31 14:19:13 +0530

    hwllo

---
+++ src/ReduxWithHooks/simpleHtml.jsx
@@ -0,0 +1,12 @@
+// import React, { useState, useEffect } from 'react';
+
+// function simpleHtml() {
+    
+//     const [ customers, setCustomers ] = useState([]);
+    
+//     return(
+        
+//     )
+// }
+
+// export const simpleHtml;
